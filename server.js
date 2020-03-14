57cd65937e3f1500bde2417edc544cd9b5134615
pb-totoro <jonesec2@gmail.com>
2020-03-13 21:08:30 -0400

    Fixed html routes commented out in server. Html working again

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // app.use(require("./routes/api"));
-// app.use(require("./routes/html"));
+app.use(require("./routes/html"));
 
 // app.get("/api/workouts", (req,res) => {
 //    db.Workout.find({}).then(data => {
