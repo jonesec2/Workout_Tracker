72ec97ad79deb09ce62f44718445b361a58c3723
pb-totoro <jonesec2@gmail.com>
2020-03-13 21:40:34 -0400

    Added id listener issues on front end

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,50 @@
 const router = require("express").Router();
 const db = require("../models");
 
-router.get("/api/workouts", (req,res) => {
+router.get("/api/workouts", (req, res) => {
+   console.log(req);
+   console.log(res);
    db.Workout.find({})
-   .then(dbWorkout => {
-      for (const workout of dbWorkout) {
-        workout.setTotalDuration();
+      .then(dbWorkout => {
+         for (const workout of dbWorkout) {
+            workout.setTotalDuration();
+         }
+         res.json(dbWorkout);
+      })
+      .catch(err => {
+         res.status(400).json(err);
+      });
+});
+
+router.post("/api/workouts", (req, res) => {
+   console.log(req);
+   console.log(res);
+   db.Workout.create(req.body)
+      .then(dbWorkout => {
+         res.json(dbWorkout);
+      })
+      .catch(err => {
+         res.json(err);
+      });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+   console.log(req.params.id);
+   console.log(req.body);
+   db.Workout.updateOne(
+      {
+         _id: req.params.id
+      },
+      {
+         $push: { exercises: req.body }
       }
-      res.json(dbWorkout);
-    })
-   .catch(err => {
-      res.status(400).json(err);
-   })
+   )
+      .then(dbWorkout => {
+         res.json(dbWorkout);
+      })
+      .catch(err => {
+         res.json(err);
+      });
 });
 
 module.exports = router;
